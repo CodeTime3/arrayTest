86bc96460eef1e4698ecefdcad24909826ef9eb6
CodeTime3 <daniele.pilloni.03@gmail.com>
2023-12-09 16:52:46 +0100

    Add project files.

---
+++ ArrayTest.Tests/FibonacciTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArrayTest.Tests
+{
+    public class FibonacciTests
+    {
+        [Theory]
+        [InlineData(5,5)]
+        [InlineData(6, 8)]
+        [InlineData(0, 0)]
+        public void FibIterTest(int iteration, int expected)
+        {
+            var result = arrayTest.Program.FibIter(iteration);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        public void FibRectTest(int iteration, int expected)
+        {
+            var result = arrayTest.Program.FibRec(iteration);
+            Assert.Equal(expected, result);
+        }
+    }
+
+}
---
+++ arrayTest/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace arrayTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = { 2, 9, -4, 6, 1, -6, 7, -8, 5, -9};
+
+            //int nums = IndexOfMin(array, 0, array.Length-1);
+            //System.Console.WriteLine(nums);
+
+            //PrintArray(array);
+            //SortArray(array);
+            //PrintArray(array);
+
+            //calcolo fattoriale
+            //4! = 4 * 3 * 2 * 1 = 24
+
+            Console.WriteLine(FactRec(5));
+            Console.WriteLine(FactIter(5));
+            Console.WriteLine(FibRec(5));
+            Console.WriteLine(FibIter(5));
+        }
+
+        static int IndexOfMin(int[] array, int from, int to) 
+        {   
+            int indexOfMin = from;
+            for (int i = from+1; i <= to; i++)
+            {
+                
+                if (array[i] < array[indexOfMin])
+                {
+                    indexOfMin = i;
+                }
+            }
+            
+            return indexOfMin; 
+        }
+
+        static void Swap(int[] array, int j, int i)
+        {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+
+        static void SortArray(int[] array)
+        {
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                int temp = IndexOfMin(array, i, array.Length-1);
+                Swap(array, i, temp);
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach(int i in array)
+            {
+                System.Console.WriteLine($"{i}, ");
+            }
+        }
+
+        static int FactRec(int num)
+        {
+            if (num == 0 || num == 1)
+            {
+                return 1;
+            }
+
+            return num * FactRec(num - 1);
+        }
+
+        static int FactIter(int num)
+        {
+            int product = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                product *= i;
+            }
+
+            return product;
+        }
+
+        public static int FibRec(int num)
+        {
+            if((num == 0) || (num == 1))
+            {
+                return num;
+            }
+
+            return FibRec(num - 1) + FibRec(num - 2);
+        }
+
+        public static int FibIter(int num)
+        {
+            if ((num == 0) || (num == 1))
+            {
+                return num;
+            }
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < num; i++)
+            {
+                var temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return a;
+        }
+    }
+}
